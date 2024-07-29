e8c29029b459ef5a2adeb779a4271025e14ff4e2
NightKnight <Ahmedatwa866@yahoo.com>
2024-07-29 20:11:54 +0300

    submit an action

--- src/components/ChakraButton/ChakraButton.stories.tsx
+++ src/components/ChakraButton/ChakraButton.stories.tsx
@@ -2,8 +2,21 @@ import { Button } from "@chakra-ui/core";
 
 export default {
     title: 'Charka/Button',
-    component: Button
+    component: Button,
+    argTypes: {
+        onClick: { action: 'clicked' }
+    }
 }
 
-export const Success = () => <Button variantColor="green">Success</Button>
-export const Danger = () => <Button variantColor="red">Danger</Button>
+const Template = args => <Button {...args} />
+
+export const Success = Template.bind({})
+Success.args = {
+    variantColor: 'green',
+    children: 'Success'
+}
+export const Danger = Template.bind({})
+Danger.args = {
+    variantColor: 'red',
+    children: 'Danger'
+}
